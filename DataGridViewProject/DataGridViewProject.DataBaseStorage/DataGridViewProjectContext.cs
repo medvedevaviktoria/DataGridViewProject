6c8edfc28318d37abc309aaa1a89ff5b29c961ce
medvedevaviktoria <medvedevaviktoria176@gmail.com>
2025-12-09 18:58:41 +0300

    добавила файл DataGridViewProjectContext.cs

--- DataGridViewProject/DataGridViewProject.DataBaseStorage/DataGridViewProjectContext.cs
+++ DataGridViewProject/DataGridViewProject.DataBaseStorage/DataGridViewProjectContext.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataGridViewProject.Constants;
+using DataGridViewProject.Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataGridViewProject.DataBaseStorage
 {
-    internal class DataGridViewProjectContext
+    public class DataGridViewProjectContext : DbContext
     {
+        /// <summary>
+        /// Сущность <see cref="ProductModel"/>.
+        /// </summary>
+        public DbSet<ProductModel> Products { get; set; }
+
+        /// <summary>
+        /// Создаёт экземпляр <see cref="DataGridViewProjectContext"/>.
+        /// </summary>
+        public DataGridViewProjectContext() =>
+            Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\mssqllocaldb;Database=DataGridViewProjectDatabase;Trusted_Connection=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductModel>(entity =>
+            {
+                // PK
+                entity.HasKey(e => e.Id);
+
+                // Наименование
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(AppConstants.ProductNameMaxLength);
+
+                // Размер
+                entity.Property(e => e.ProductSize)
+                    .IsRequired()
+                    .HasMaxLength(AppConstants.ProductSizeMaxLength);
+
+                // enum Material как int
+                entity.Property(e => e.Material)
+                    .HasConversion<int>();
+
+                // Количества
+                entity.Property(e => e.Quantity);
+                entity.Property(e => e.MinQuantity);
+
+                // Цена
+                entity.Property(e => e.PriceWithoutTax);
+            });
+        }
+
     }
 }
